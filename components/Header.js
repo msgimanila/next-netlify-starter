34ecba852bbd56dbb680885ab1c1b24f20e80408
Mario Garcia <msgimanila@yahoo.com>
2023-10-07 23:07:20 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,3 @@
 export default function Header({ title, subtitle }) {
-  return <h1 className="title">{title}</h1> <h1 className="title">{subtitle}</h1>
+  return <h1 className="title">{title}</h1>  
 }
