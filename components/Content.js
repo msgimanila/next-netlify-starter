053d30dd332b82a6741c408b87d83a7bf9d8f9aa
Mario Garcia <msgimanila@yahoo.com>
2023-10-07 22:41:54 +0800

    Update Content.js

--- components/Content.js
+++ components/Content.js
@@ -1,10 +1,39 @@
+// Chat.js
+import React, { useState, useEffect } from 'react';
 
-import styles from './Footer.module.css'
+function Content() {
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState('');
 
-export default function Content() {
+  const handleNewMessageChange = (event) => {
+    setNewMessage(event.target.value);
+  };
+
+  const handleSendMessage = () => {
+    if (newMessage.trim() !== '') {
+      setMessages([...messages, { text: newMessage }]);
+      setNewMessage('');
+    }
+  };
+
   return (
-    <>
-      <div>CONTENT</div>
-    </>
-  )
+    <div>
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>{message.text}</div>
+        ))}
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Type your message..."
+          value={newMessage}
+          onChange={handleNewMessageChange}
+        />
+        <button onClick={handleSendMessage}>Send</button>
+      </div>
+    </div>
+  );
 }
+
+export default Chat;
